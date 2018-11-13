de7925c122fdca29fcc8bed278cea4f3732797eb
zhangzhen <zzhang@gaojihealth.com>
2018-11-13 17:23:32 +0800

    asd

---
+++ provider/src/main/java/com/provider/Swagger2.java
@@ -0,0 +1,36 @@
+package com.provider;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2 // 启用Swagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {// 创建API基本信息
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.provider.controller"))// 扫描该包下的所有需要在Swagger中展示的API，@ApiIgnore注解标注的除外
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {// 创建API的基本信息，这些信息会在Swagger UI中进行显示
+        return new ApiInfoBuilder()
+                .title("demo")// API 标题
+                .description("二狗蛋子")// API描述
+                .contact("zz@")// 联系人
+                .version("1.0")// 版本号
+                .build();
+    }
+
+}
